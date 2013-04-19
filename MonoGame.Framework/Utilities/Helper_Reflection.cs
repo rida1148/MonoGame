79ef79547dd542779688944af40dab3919be6700
Simon (darkside) Jackson <darkside@zenithmoon.com>
2013-04-19 15:58:02 +0100

    Current Work in progress Helper classes
    * Helper_File.cs handled File operations, function scope almost complete, support Windows / Windows Phone and Android at present
    
    * Helper_Reflection.cs - just started so not much in there yet
    
    All functions tested on supported platforms in a seperate Dev branch, so this branch just contains the classes themselves for inspection

---
+++ MonoGame.Framework/Utilities/Helper_Reflection.cs
@@ -0,0 +1,32 @@
+﻿
+using Microsoft.Xna.Framework.Content;
+using System;
+using System.IO;
+#if WINRT
+using System.Reflection;
+#endif
+
+namespace Microsoft.Xna.Framework.Utilities
+{
+    internal static class Helper_Reflection
+    {
+#if WINRT
+        internal static char notSeparator = '/';
+        internal static char separator = '\\';
+#else
+        internal static char notSeparator = '\\';
+        internal static char separator = Path.DirectorySeparatorChar;
+#endif
+
+        internal static bool IsValueType(ContentTypeReader typeReader)
+        {
+#if WINRT
+            return !typeReader.TargetType.GetTypeInfo().IsValueType;
+#else
+            return !typeReader.TargetType.IsValueType;
+#endif
+           
+        }
+
+    }
+}
